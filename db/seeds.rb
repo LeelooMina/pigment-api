e0d1b24a6909d227346e8d1a860f5a5e111c7cab
Crystal Campbell <blankpage@gmail.com>
2023-03-25 09:53:20 -0500

    chore: seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Pigment.create(name: "PR83", description: "Alizarin Crimson". available: true)
+Pigment.create(name: "PR83", description: "Alizarin Crimson", available: true)
 
-Pigment.create(name: "PR108", description: "Cadmium Red". available: true)
+Pigment.create(name: "PR108", description: "Cadmium Red", available: true)
 
-Pigment.create(name: "PV19", description: "Carmine". available: true)
+Pigment.create(name: "PV19", description: "Carmine", available: true)
 
-Pigment.create(name: "PB28", description: "Cobalt Blue". available: true)
+Pigment.create(name: "PB28", description: "Cobalt Blue", available: true)
 
-Pigment.create(name: "PBr7", description: "Burnt Sienna:". available: true)
+Pigment.create(name: "PBr7", description: "Burnt Sienna:", available: true)
 
-Pigment.create(name: "PB35", description: "Cerulean Blue". available: true)
+Pigment.create(name: "PB35", description: "Cerulean Blue", available: true)
 
-Pigment.create(name: "PY151", description: "Gamboge". available: true)
+Pigment.create(name: "PY151", description: "Gamboge", available: true)
 
-Pigment.create(name: "PG7", description: "Hooker's Green". available: true)
+Pigment.create(name: "PG7", description: "Hooker's Green", available: true)
 
-Pigment.create(name: "PR101", description: "Indian Red:". available: true)
+Pigment.create(name: "PR101", description: "Indian Red:", available: true)
 
-Pigment.create(name: "PB66", description: "Indigo". available: true)
+Pigment.create(name: "PB66", description: "Indigo", available: true)
 
-Pigment.create(name: "PBk9", description: "Ivory Black". available: true)
+Pigment.create(name: "PBk9", description: "Ivory Black", available: true)
