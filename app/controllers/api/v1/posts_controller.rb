9d91aca34683d2d2c4952d1935bf9c5e5e1e1618
Crystal Campbell <blankpage@gmail.com>
2023-03-27 11:59:32 -0500

    Chore: allow guests to view posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,56 +1,59 @@
 module Api
   module V1
     class PostsController < ApplicationController
-  before_action :set_post, only: %i[ show update destroy ]
+      before_action :set_post, only: %i[ show update destroy ]
+      skip_before_action :authenticate, only: %i[ index ]
 
-  # GET /posts
-  def index
-    @posts = Post.all
+      # GET /posts
+      def index
+        @posts = Post.all
 
-    render json: @posts
-  end
+        render json: @posts
+      end
 
-  # GET /posts/1
-  def show
-    render json: @post
-  end
+      # GET /posts/1
+      def show
+        render json: @post
+      end
 
-  # POST /posts
-  def create
-    @post = Post.new(post_params)
+      # POST /posts
+      def create
+        # @post = Post.new(post_params)
 
-    if @post.save
-      render json: @post, status: :created, location: @post
-    else
-      render json: @post.errors, status: :unprocessable_entity
+        @post = @current_user.posts.build(post_params)
+
+        if @post.save
+          render json: @post, status: :created
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /posts/1
+      def update
+        if @post.update(post_params)
+          render json: @post
+        else
+          render json: @post.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /posts/1
+      def destroy
+        @post.destroy
+      end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_post
+        @post = Post.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def post_params
+        params.require(:post).permit(:user_id, :title, :content, :img_url)
+      end
     end
   end
-
-  # PATCH/PUT /posts/1
-  def update
-    if @post.update(post_params)
-      render json: @post
-    else
-      render json: @post.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /posts/1
-  def destroy
-    @post.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:user_id, :title, :content, :img_url)
-    end
-end
-
-end
 end
