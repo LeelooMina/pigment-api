239815edf5a2cbed226842603b4c73d57d22990b
Crystal Campbell <blankpage@gmail.com>
2023-03-27 11:59:51 -0500

    fix: syntax issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :content :title, presence: true, length: {minimum: 2, mexium: 100}
+  validates :content, :title, presence: true, length: {minimum: 2, mexium: 100}
 
   has_many :likes
-  has_many :comments, through: => :comment_post
-  has_one :palette, through: => :palette_post
+  has_many :comments, through: :comment_post
+  has_one :palette, through: :palette_post
 
 
 
