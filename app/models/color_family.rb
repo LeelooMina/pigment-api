a1b12c8e70cace9a3b561562771f99ba79485018
Crystal Campbell <blankpage@gmail.com>
2023-04-13 20:14:07 -0500

    color family fix attempt

--- app/models/color_family.rb
+++ app/models/color_family.rb
@@ -1,6 +1,7 @@
 class ColorFamily < ApplicationRecord
-    has_many :pigments, :through => :color_family_pigments
-
+    has_many :color_family_pigments
+    has_many :pigments, through: :color_family_pigments
     has_one_attached :image
+
     
 end
