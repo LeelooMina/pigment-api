bb846b7be5bc38bea35359dac950bb7c1de31a0d
Crystal Campbell <blankpage@gmail.com>
2023-04-17 16:15:58 -0500

    Update paint.rb

--- app/models/paint.rb
+++ app/models/paint.rb
@@ -3,4 +3,5 @@ class Paint < ApplicationRecord
   has_many :paint_pigments
   has_many :pigments, through: :paint_pigments
   has_one :swatch, through: :color_family_pigments, source: :swatch
+  belongs_to :color_family
 end
